debc171c043aa20a2ef285e946b109e0a887724e
kevin <korgon@gbook.notamiss.com>
2017-05-05 00:31:40 -0600

    Konva progress...

--- lib/vue/src/components/controller/controller.vue
+++ lib/vue/src/components/controller/controller.vue
@@ -1,32 +1,16 @@
 <template>
-	<div class="controller-wrapper">
-		<div class="controller-left">
-			<div class="controller-grid-wrapper"><grid :axii="axii" :ratio="ratio" :width="width" :height="height" :wrapperWidth="wrapperWidth"></grid></div>
-			<div class="controller-slider-horizontal"><slider :axis="pan" @move="moveAxis"></slider></div>
-		</div>
-		<div class="controller-right">
-			<div class="controller-slider-vertical"><slider :axis="tilt" @move="moveAxis" :width="width" :height="height" :wrapperWidth="wrapperWidth"></slider></div>
-		</div>
-	</div>
+	<div id="controller-window"></div>
 </template>
 
 <script>
-	// components
-	var slider = require('./slider.vue');
-	var grid = require('./grid.vue');
-
 	/*
 		New ideas...
-		Don't use canvas library; instead use a div
-		use range slider for precision control
+		Don't use range slider use konva for better accuracy in drawings...
 	*/
+	var controller = require('./controller.js');
 
 	module.exports = {
 		name: 'controller',
-		components: {
-			grid,
-			slider
-		},
 		props: ['axii'],
 		data: function() {
 			return {
@@ -34,25 +18,21 @@
 				tilt: this.axii.tilt,
 				width: undefined,
 				height: undefined,
-				wrapperWidth: undefined,
-				ratio: undefined
+				ratio: undefined,
+				invertX: false,
+				invertY: false
 			}
 		},
 		created: function() {
 			var self = this;
 
-			setTimeout(function() {
-				resizeController();
-			});
-
+			// adjust variables when window is resized
 			window.addEventListener('resize', resizeController);
 
 			function resizeController() {
-				var grid = document.querySelector('.controller-grid');
-				var controllerWrap = document.querySelector('.controller-wrapper');
+				var grid = document.getElementById('controller-window');
 
 				self.width = grid && Math.floor(grid.getBoundingClientRect().width);
-				self.wrapperWidth = controllerWrap && Math.floor(controllerWrap.getBoundingClientRect().width);
 
 				// calculate ratio to determine height of grid
 				var panRange = self.axii.pan.bounds.range;
@@ -61,38 +41,28 @@
 				console.log('ratio:', self.ratio);
 
 				self.height = tiltRange * self.ratio;
+
+				controller.resizeStage(self.width);
 			}
+
+			setTimeout(function() {
+				resizeController();
+				controller.initialize(self);
+			});
 		},
 		methods: {
-			moveAxis: function(axis) {
-				console.log('MOVING', axis);
-				this.axii[axis.name].current.position = axis.position;
+			move: function() {
+				console.log('MOVING!!!!!!!!!');
 			}
+		},
+		destroyed: function() {
+			controller.unload();
 		}
 	}
 </script>
 
 <style lang="scss">
-	.controller-wrapper {
-		display: flex;
-	}
+	#controller-window {
 
-	.controller-left {
-		flex: 3 0;
-		.controller-slider-horizontal {
-			margin-top: 10px;
-		}
-	}
-
-	.controller-right {
-		flex: 0 0 40px;
-		.controller-slider-vertical {
-			position: relative;
-			height: 100%;
-		}
-	}
-
-	.controller-grid-wrapper {
-		
 	}
 </style>
